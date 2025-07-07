16902a4d6f98e16058a9cb77621cb204ca82ff12
Dhivyalakshmi Kumaran <dhivyalax14@gmail.com>
2025-07-07 14:39:03 +0530

    Create index.js
    starting concept example

---
+++ index.js
@@ -0,0 +1,9 @@
+var a = 10;
+function square(x){
+    var b = x;
+    return b*b;
+}
+var result1 = square(a);
+var result2 = square(5);
+console.log("square of " + a + "is:"+ result1);
+console.log("square of 5 is :" + result2);
